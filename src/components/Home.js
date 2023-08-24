bf94fd377d6c35d22558641cb2c1ba91f26ac3ca
Xudiyef21 <xeyalxeyal068@gmail.com>
2023-08-24 20:28:43 +0400

    first commit

---
+++ src/components/Home.js
@@ -0,0 +1,51 @@
+/** @format */
+
+import React from "react";
+import Logo from "../assets/photo.jpg";
+import { MdOutlineKeyboardArrowRight } from "react-icons/md";
+import { Link } from "react-scroll";
+
+const Home = () => {
+  return (
+    <div
+      name="home"
+      className="w-full bg-gradient-to-b from-black to-gray-800  via-black max-md:pt-24 text-center  md:h-screen mt-0 "
+    >
+      <div className="max-w-screen-lg mx-auto flex flex-col items-center justify-center h-full px-4 md:flex-row">
+        <div className="flex flex-col justify-center h-full mt-20 ">
+          <h2 className=" text-4xl sm:text-5xl font-bold max-sm::text-center text-white md:text-6xl text-start">
+            I'm a Front-End developer
+          </h2>
+          <p className="text-neutral-400 py-4 max-w-md">
+            A front-End developer passionate about creative interactive
+            applications and experiences on web.
+          </p>
+          <div class="flex justify-center items-center " >
+            <Link
+              to="portfolio"
+              smooth
+              duration={500}
+              className="group text-white w-fit px-6 py-3 my-4 flex items-center justify-center rounded-md bg-gradient-to-r from-cyan-500 to-blue-500 cursor-pointer pl"
+            >
+              Portfolio
+              <span className="group-hover:rotate-90 duration-300">
+                <MdOutlineKeyboardArrowRight size={20} />
+              </span>
+            </Link>
+          </div>
+        </div>
+
+        <div className="w-3/4 md:1/4 mb-5  ">
+          <img
+            src={Logo}
+            alt="my profile "
+            className="rounded-2xl ms-auto
+              w-full object-cover"
+          />
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default Home;
---
+++ src/components/Portfolio.js
@@ -0,0 +1,94 @@
+/** @format */
+
+import React from "react";
+import Alqoritmika from "../assets/Portfolioİmg/algoritmika.png";
+import ToDo from "../assets/Portfolioİmg/todo.png";
+import Converter from "../assets/Portfolioİmg/converter.png";
+import EmojiRating from "../assets/Portfolioİmg/emojiRating.png";
+import PortfolioImg from "../assets/Portfolioİmg/portfolio.png";
+import OTPform from "../assets/Portfolioİmg/otpform.png";
+
+const Portfolio = () => {
+  const portfolios = [
+    {
+      id: 1,
+      src: Alqoritmika,
+      codeUrl: "https://github.com/xayalxudiyev/1.Alqoritmika.git",
+      demoUrl: "https://xayalxudiyev.github.io/1.Alqoritmika/",
+    },
+    {
+      id: 2,
+      src: ToDo,
+      codeUrl: "https://github.com/xayalxudiyev/2.TO-DO-list.git",
+      demoUrl: "https://xayalxudiyev.github.io/2.TO-DO-list/",
+    },
+    {
+      id: 3,
+      src: Converter,
+      codeUrl: "https://github.com/xayalxudiyev/3.CONVERTER.git",
+      demoUrl: "https://xayalxudiyev.github.io/3.CONVERTER/",
+    },
+    {
+      id: 4,
+      src: EmojiRating,
+      codeUrl: "https://github.com/xayalxudiyev/4.Emoji_rating.git",
+      demoUrl: "https://xayalxudiyev.github.io/4.Emoji_rating/",
+    },
+    {
+      id: 5,
+      src: PortfolioImg,
+      codeUrl: "https://github.com/xayalxudiyev/Portfolio",
+      demoUrl: "https://xayalxudiyev.github.io/Portfolio/",
+    },
+    {
+      id: 6,
+      src: OTPform,
+      codeUrl: "https://github.com/xayalxudiyev/OTP-Verificitaion-Form",
+      demoUrl: "https://xayalxudiyev.github.io/OTP-Verificitaion-Form/",
+    },
+  ];
+
+  return (
+    <div
+      name="portfolio"
+      className="w-full h-full bg-gradient-to-b from-black to-gray-800  text-white pt-16"
+    >
+      <div className="max-w-screen-lg p-4 mx-auto flex flex-col justify-between    w-full h-full">
+        <div className="pb-8">
+          <p className="text-4xl font-bold inline border-b-4 border-gray-500">
+            Portfolio
+          </p>
+          <p className="py-6">Check Out Some Of My Work Right Here</p>
+        </div>
+
+        <div className=" pb-16 grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 gap-8 px-12 sm:px-0 flex-wrap">
+          {portfolios.map(({ id, src, codeUrl, demoUrl }) => (
+            <div key={id}>
+              <div className="shadow-md shadow-gray-600 rounded-lg ">
+                <img
+                  src={src}
+                  alt="Portfolio item"
+                  className="rounded-md duration-200 hover:scale-105"
+                />
+                <div className="flex justify-center items-center">
+                  <button className=" w-1/2 px-6 py-3 m-4 duration-200 hover:scale-105">
+                    <a href={demoUrl} target="_blank">
+                      Demo
+                    </a>
+                  </button>
+                  <button className=" w-1/2 px-6 py-3 m-4 duration-200 hover:scale-105">
+                    <a href={codeUrl} target="_blank">
+                      Code
+                    </a>
+                  </button>
+                </div>
+              </div>
+            </div>
+          ))}
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default Portfolio;
